f3183b9e56be2dda2dab562cb5fd1e4f1c50ba82
David Laing <david.laing@alumni.ubc.ca>
2016-12-17 20:03:28 -0500

    fixed typos

--- iso_triangle_class.R
+++ iso_triangle_class.R
@@ -1,25 +1,25 @@
-#' An S3 class to represent a triangle.
-#' Triangle
+#' An S3 class to represent an isosceles triangle.
+#' Isosceles triangle
 #' @param base A number.
 #' @param height A number.
-#' @return A triangle object with a base and a height.
+#' @return An isosceles triangle object with a base and a height.
 #' @export
 #' @examples
-#' triangle(4,5)
+#' iso_triangle(4,5)
 
-#' triangle area() function
+#' iso_triangle area() function
 #' 
-#' @return The area of a triangle with base \code{base} and height \code{height}.
+#' @return The area of an isosceles triangle with base \code{base} and height \code{height}.
 #' @examples
 #' area()
 
-#' triangle perimeter() function
+#' iso_triangle perimeter() function
 #' 
-#' @return The perimeter of a triangle with base \code{base} and height \code{height}.
+#' @return The perimeter of an isosceles triangle with base \code{base} and height \code{height}.
 #' @examples
 #' perimeter()
 
-mytriangle <- triangle(3,5)
+mytriangle <- iso_triangle(3,5)
 mytriangle
-area.triangle(mytriangle)
-perimeter.triangle(mytriangle)
+area.iso_triangle(mytriangle)
+perimeter.iso_triangle(mytriangle)
