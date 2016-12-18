4d2ff60f7914d9118602c73b57e614b04031fa56
David Laing <david.laing@alumni.ubc.ca>
2016-12-17 19:41:27 -0500

    switched to base times height

--- triangle_class.R
+++ triangle_class.R
@@ -1,26 +1,25 @@
 #' An S3 class to represent a triangle.
 #' Triangle
-#' @param side_length_1 A number.
-#' @param side_length_2 A number.
-#' @param side_length_3 A number.
-#' @return A triangle object with three side lengths.
+#' @param base A number.
+#' @param height A number.
+#' @return A triangle object with a base and a height.
 #' @export
 #' @examples
-#' triangle(c(4,5,2))
+#' triangle(4,5)
 
 #' triangle area() function
 #' 
-#' @return The area of a triangle with side lengths \code{side_length_1}, \code{side_length_2}, and \code{side_length_3}.
+#' @return The area of a triangle with base \code{base} and height \code{height}.
 #' @examples
 #' area()
 
-#' rectangle perimeter() function
+#' triangle perimeter() function
 #' 
-#' @return The perimeter of a triangle with side lengths \code{side_length_1}, \code{side_length_2}, and \code{side_length_3}.
+#' @return The perimeter of a triangle with base \code{base} and height \code{height}.
 #' @examples
 #' perimeter()
 
 mytriangle <- triangle(3,5,2)
 mytriangle
 area.triangle(mytriangle)
-perimeter.rectangle(mytriangle)
+perimeter.triangle(mytriangle)
